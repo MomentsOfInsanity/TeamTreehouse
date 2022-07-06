880da4fbf5f0553458bd1ce79b4a654ce10481b6
Raj Bedi <raj.j.bedi@gmail.com>
2022-07-06 15:42:13 -0400

    Floats

--- PHP Basics/numbers.php
+++ PHP Basics/numbers.php
@@ -3,11 +3,16 @@ $num_one = 1;
 $num_two = 2;
 $num_three = 3;
 
-// echo $num_one;
+/*// echo $num_one;
 var_dump($num_one); 
 // echo 1;
 var_dump(1);
 // echo "1";
 var_dump("1");
-var_dump($num_one + $num_two - $num_three);
+var_dump($num_one + $num_two - $num_three);*/
+
+$distance_home = 1.2;
+$distance_work = 2.5;
+
+var_dump($distance_home + $distance_work + $num_three + .3);
 ?>
