ef178efa58be1ebdbbca595b525737db2591bd45
Raj Bedi <raj.j.bedi@gmail.com>
2022-07-06 15:51:09 -0400

    Arithmetic Operators

--- PHP Basics/numbers.php
+++ PHP Basics/numbers.php
@@ -15,4 +15,25 @@ $distance_home = 1.2;
 $distance_work = 2.5;
 
 var_dump($distance_home + $distance_work + $num_three + .3);
+
+$a = 5;
+$b = 10;
+
+var_dump($a * $b);
+var_dump($a / $b);
+
+$a = $a +1;
+var_dump($a);
+$a++;
+var_dump($a);
+$a--;
+var_dump($a);
+var_dump($a--);
+var_dump($a);
+var_dump(--$a)
+var_dump($a);
+$a = $a + 5;
+var_dump($a);
+$a += 5;
+var_dump($a);
 ?>
