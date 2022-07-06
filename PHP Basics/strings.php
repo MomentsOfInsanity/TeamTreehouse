8a334c777eb089cead06580984a3e7ca54ac945c
Raj Bedi <raj.j.bedi@gmail.com>
2022-07-06 16:58:16 -0400

    Combining Strings

--- PHP Basics/strings.php
+++ PHP Basics/strings.php
@@ -1,5 +1,11 @@
 <?php
 $name = 'Raj Bedi';
 $string_one = "Learning to display \"Hello $name!\" to the screen.\n";
+$string_one = 'Learning to display ';
+$string_one .= '"Hello ';
+$string_one .= $name;
+$string_one .= '!" to the screen.';
+$string_one = $string_one . "\n";
+$string_one = 'I am ' . $string_one;
 echo $string_one;
 ?>
