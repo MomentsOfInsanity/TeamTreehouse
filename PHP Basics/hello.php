65a782c678b5ba6e4f1b4ea25a3cfce43ed55734
Raj Bedi <raj.j.bedi@gmail.com>
2022-07-06 15:20:12 -0400

    PHP Comments

--- PHP Basics/hello.php
+++ PHP Basics/hello.php
@@ -1,1 +1,23 @@
-<?php echo 'Hello World!'; ?>
+<?php
+/*
+    * This file is our first "Hello World!" Script
+    *
+    * @author: Raj Bedi
+*/
+
+
+//our first php script
+echo 'Hello World!'; //done
+//echo 'Hello Treehouse!'; //done
+
+// You can make a single line comment as long as you want, even wrapping to a new line, as long as you dont add a hard return
+//This is a second comment line
+
+/*
+line 1
+line 2
+line 3
+*/
+
+
+?>
