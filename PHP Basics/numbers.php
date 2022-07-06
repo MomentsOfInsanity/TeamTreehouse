d197a4341cb42354aaa9a4cb35081c5a00dd1f75
Raj Bedi <raj.j.bedi@gmail.com>
2022-07-06 15:33:11 -0400

    Integers

---
+++ PHP Basics/numbers.php
@@ -0,0 +1,13 @@
+<?php 
+$num_one = 1;
+$num_two = 2;
+$num_three = 3;
+
+// echo $num_one;
+var_dump($num_one); 
+// echo 1;
+var_dump(1);
+// echo "1";
+var_dump("1");
+var_dump($num_one + $num_two - $num_three);
+?>
