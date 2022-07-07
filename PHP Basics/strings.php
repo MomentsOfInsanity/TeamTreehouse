ca86a5c54f21fc50cc45605e98fe4b6d81c906d8
Raj Bedi <raj.j.bedi@gmail.com>
2022-07-07 16:06:58 -0400

    Comparing Values

--- PHP Basics/strings.php
+++ PHP Basics/strings.php
@@ -5,13 +5,24 @@ $string_one = 'Learning to display ';
 $string_one .= '"Hello ';
 $string_one .= $name;
 $string_one .= '!" to the screen.';
-$string_one = $string_one . "\n";
-$string_one = 'I am ' . $string_one;
+// $string_one = $string_one . "\n";
+//prepend to the string
+// $string_one = 'I am ' . $string_one;
 echo $string_one;
 
 $isReady = true;
-var_dump($isReady);
+// var_dump($isReady);
 $isReady = false;
-var_dump($isReady);
+// var_dump($isReady);
+
+var_dump(1 + "2");
+
+$a = 10;
+$b = "10";
+
+// var_dump($a == $b);
+// var_dump($a === $b);
+
+var_dump($string_one === 'Learning to display "Hello Raj Bedi!" to the screen.');
 ?>
 
