624fddbba981ba6662c2a521a11bd36083da7808
Raj Bedi <raj.j.bedi@gmail.com>
2022-07-21 16:04:06 -0400

    Associative Arrays

---
+++ PHP Arrays and Control Structures/associative_arrays.php
@@ -0,0 +1,10 @@
+<?php
+
+$iceCream = array(
+    'Alena' => 'Black Cherry', 
+    'Treasure' => 'Chocolate', 
+    'Dave' => 'Cookies and Cream', 
+    'Rialle' => 'Strawberry');
+
+echo $iceCream['Alena'];
+?>
