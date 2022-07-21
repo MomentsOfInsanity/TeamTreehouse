8f3aaa081665fbad913ab9cb7ab1fbd20326109c
Raj Bedi <raj.j.bedi@gmail.com>
2022-07-21 16:30:25 -0400

    Multidimensional Arrays

---
+++ PHP Arrays and Control Structures/todo.php
@@ -0,0 +1,20 @@
+<?php
+
+$list[] = [
+    'title' => 'Laundry',
+    'priority' => '2',
+    'due' => '',
+    'completed' => false,
+];
+$list[] = [
+    'title' => 'Clean out refrigerator',
+    'priority' => '3',
+    'due' => '07/30/2016',
+    'completed' => false,
+];
+// $list = array($task1, $task2);
+var_dump($list);
+// echo $list[0]['title'];
+// var_dump($task1);
+
+?>
