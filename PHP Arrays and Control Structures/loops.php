49dc999316163a1f4fe9a0fa5801a389215f5a47
Raj Bedi <raj.j.bedi@gmail.com>
2022-07-22 10:23:55 -0400

    Do While Conditioning

---
+++ PHP Arrays and Control Structures/loops.php
@@ -0,0 +1,16 @@
+<?php
+
+$currentYear = date('Y');
+$year = $currentYear - 100;
+
+// while ($year <= $currentYear) {
+//     echo $year . '<br />\n';
+//     $year++;
+// }
+
+do {
+    echo $year . '<br />\n';
+    while (++$year <= $currentYear);
+}
+
+?>
