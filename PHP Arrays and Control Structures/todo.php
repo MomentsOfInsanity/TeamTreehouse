370876115a17991e5903e2ae5ff9b42c1a3e0582
Raj Bedi <raj.j.bedi@gmail.com>
2022-07-22 13:18:32 -0400

    for each loops

--- PHP Arrays and Control Structures/todo.php
+++ PHP Arrays and Control Structures/todo.php
@@ -1,17 +1,32 @@
 <?php
 
-$list[] = [
-    'title' => 'Laundry',
-    'priority' => '2',
-    'due' => '',
-    'completed' => false,
-];
-$list[] = [
-    'title' => 'Clean out refrigerator',
-    'priority' => '3',
-    'due' => '07/30/2016',
-    'completed' => false,
-];
+include 'list.php';
+
+foreach ($list as $key =>$item) {
+    echo $key . ' = ' . $item['title'] . '<br />\n';
+}
+
+echo '<table>';
+echo '<tr>';
+echo '<th>Title</th>';
+echo '<th>Priority</th>';
+echo '<th>Due Date</th>';
+echo '<th>Compplete</th>';
+echo '</tr>';
+foreach ($list as $item) {
+    echo '<tr>';
+    echo '<td>' . $item['title'] . '</td>\n';
+    echo '<td>' . $item['priority'] . '</td>\n';
+    echo '<td>' . $item['due'] . '</td>\n';
+    echo '<td>';
+    if ($item['complete']) {
+        echo 'Yes';
+    } else {
+        echo 'No';
+    }
+    echo '</td>\n';
+    echo '</tr>';
+}
 // $list = array($task1, $task2);
 var_dump($list);
 // echo $list[0]['title'];
