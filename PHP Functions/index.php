e47bd4084019011824952dec1d28c408e5dd38ee
Raj Bedi <raj.j.bedi@gmail.com>
2022-07-25 09:39:39 -0400

    PHP Function Default Arguments

--- PHP Functions/index.php
+++ PHP Functions/index.php
@@ -1,21 +1,31 @@
 <?php 
 
-function hello($arr) {
-    if(is_array($arr)) {
-        foreach($arr as $name) {
-            echo "Hello, $name, How's it going!<br>";
-        }
+function get_info($name, $title = Null) {
+    if ($title) {
+        echo "$name has arrived, they are with us as a $title";
     } else {
-        echo "Hello, friends!";
+        echo "$name had arrived, welcome!";
     }
-}   
+}
 
-$names = array(
-    "John",
-    "Jane",
-    "Bob"
-);
-hello($names);
+get_info('Mike', 'frog');
+
+// function hello($arr) {
+//     if(is_array($arr)) {
+//         foreach($arr as $name) {
+//             echo "Hello, $name, How's it going!<br>";
+//         }
+//     } else {
+//         echo "Hello, friends!";
+//     }
+// }   
+
+// $names = array(
+//     "John",
+//     "Jane",
+//     "Bob"
+// );
+// hello($names);
 
 // $current_user = 'Mike';
 
