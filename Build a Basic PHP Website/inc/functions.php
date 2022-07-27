632aac1d0c5ae99ceb6436aea47fcfaf2eb49a99
Raj Bedi <raj.j.bedi@gmail.com>
2022-07-27 11:18:44 -0400

    Displaying Categories

--- Build a Basic PHP Website/inc/functions.php
+++ Build a Basic PHP Website/inc/functions.php
@@ -9,4 +9,18 @@ function get_item_html($id, $item) {
     return $output;
 }
 
+function array_category($catalog, $category) {
+    if ($category == null) {
+        return array_keys($catalog);
+    }
+
+    $output = array();
+    foreach($catalog as $id => $item) {
+        if(strtolower($item["category"]) == strtolower($category)) {
+            $output[] = $id;
+        }
+    }
+    return $output;
+}
+
 ?>
